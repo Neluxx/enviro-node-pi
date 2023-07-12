70e65154ce1adee3d1086c0b220e0d257df19b5e
Fabian Arndt <fabian.arndt96@proton.me>
2023-07-12 11:21:55 +0200

    Insert sensor data into database

--- main.py
+++ main.py
@@ -8,21 +8,54 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 
+import os
 import mh_z19
+from datetime import datetime
+import mysql.connector
 
 
-def get_sensor_data() -> None:
+def get_sensor_data():
     """Get data from sensor"""
+
     data = mh_z19.read_all()
     save_sensor_data(data)
 
 
-def save_sensor_data(data) -> None:
+def save_sensor_data(data):
     """Save sensor data into database"""
-    co2 = data["co2"]
-    temperature = data["temperature"]
-    print("CO2: ", co2)
-    print("Temperature: ", temperature)
+
+    data = {
+        "value": data["co2"],
+        "created": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    }
+
+    insert_sensor_data(data)
+
+
+def insert_sensor_data(data):
+    """Insert sensor data to database"""
+
+    dbconnect = create_database_connection()
+
+    cursor = dbconnect.cursor()
+
+    sql = "INSERT INTO co2 (value, created) VALUES (%s, %s)"
+    val = (data["value"], data["created"])
+    cursor.execute(sql, val)
+
+    dbconnect.commit()
+    dbconnect.close()
+
+
+def create_database_connection():
+    """Create database connection"""
+
+    return mysql.connector.connect(
+        host=os.getenv("HOST"),
+        user=os.getenv("USERNAME"),
+        password=os.getenv("PASSWORD"),
+        database=os.getenv("DATABASE"),
+    )
 
 
 if __name__ == "__main__":
