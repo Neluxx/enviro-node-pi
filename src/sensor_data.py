c1969ad4699b619d55b4734cfb82e85034d57326
Fabian Arndt <fabian.arndt96@proton.me>
2023-08-04 22:03:23 +0200

    Remove sea level pressure from constructor

--- src/sensor_data.py
+++ src/sensor_data.py
@@ -13,13 +13,13 @@ from datetime import datetime
 class SensorData:
     """Sensor Data Class"""
 
-    def __init__(self, sea_level_pressure):
+    def __init__(self):
         self.mhz19 = mh_z19.read_all()
         self.i2c = I2C(board.SCL, board.SDA)
         self.bme680 = adafruit_bme680.Adafruit_BME680_I2C(self.i2c, debug=False)
 
         self.temperature_offset = -7.5
-        self.bme680.sea_level_pressure = sea_level_pressure if sea_level_pressure is not None else 1013.25
+        self.bme680.sea_level_pressure = 1013.25
 
     def get_data(self):
         """Get data from sensors"""
