51f3fe9c94acdb6e67903aab5c1a411784c055ff
Fabian Arndt <fabian.arndt96@proton.me>
2023-08-05 10:59:22 +0200

    Add script to debug bme680 sensor

---
+++ bme680.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""
+BME680
+"""
+
+import adafruit_bme680
+import bme680
+import board
+import time
+
+
+def run():
+    """Main Method"""
+
+    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+
+    sensor.set_humidity_oversample(bme680.OS_2X)
+    sensor.set_pressure_oversample(bme680.OS_4X)
+    sensor.set_temperature_oversample(bme680.OS_8X)
+    sensor.set_filter(bme680.FILTER_SIZE_3)
+
+    sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+    sensor.set_gas_heater_temperature(320)
+    sensor.set_gas_heater_duration(150)
+    sensor.select_gas_heater_profile(0)
+
+    if sensor.get_sensor_data():
+        print("BME680")
+        output = "{0:.2f} C,{1:.2f} hPa,{2:.2f} %RH".format(sensor.data.temperature, sensor.data.pressure, sensor.data.humidity)
+
+        if sensor.data.heat_stable:
+            print("{0},{1} Ohms".format(output, sensor.data.gas_resistance))
+
+        else:
+            print(output)
+
+    # Create sensor object, communicating over the board's default I2C bus
+    i2c = board.I2C()   # uses board.SCL and board.SDA
+    bme680 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
+
+    # change this to match the location's pressure (hPa) at sea level
+    bme680.sea_level_pressure = 1013.25
+
+    print("Adafruit BME680")
+    print("\nTemperature: %0.1f C" % bme680.temperature)
+    print("Gas: %d ohm" % bme680.gas)
+    print("Humidity: %0.1f %%" % bme680.relative_humidity)
+    print("Pressure: %0.3f hPa" % bme680.pressure)
+    print("Altitude = %0.2f meters" % bme680.altitude)
