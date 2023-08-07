35aa91c765221a71b46f9c886d2399b317cea998
Fabian Arndt <fabian.arndt96@proton.me>
2023-08-07 15:21:40 +0200

    Remove gas resistance output

--- debug_bme680.py
+++ debug_bme680.py
@@ -9,27 +9,26 @@ import bme680
 def run():
     """Main Method"""
 
-    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+    try:
+        sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+    except (RuntimeError, IOError):
+        sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+
+    # These oversampling settings can be tweaked to
+    # change the balance between accuracy and noise in
+    # the data.
 
     sensor.set_humidity_oversample(bme680.OS_2X)
     sensor.set_pressure_oversample(bme680.OS_4X)
     sensor.set_temperature_oversample(bme680.OS_8X)
     sensor.set_filter(bme680.FILTER_SIZE_3)
 
-    sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
-    sensor.set_gas_heater_temperature(320)
-    sensor.set_gas_heater_duration(150)
-    sensor.select_gas_heater_profile(0)
-
     if sensor.get_sensor_data():
-        print("BME680")
-        output = "{0:.2f} C,{1:.2f} hPa,{2:.2f} %RH".format(sensor.data.temperature, sensor.data.pressure, sensor.data.humidity)
-
-        if sensor.data.heat_stable:
-            print("{0},{1} Ohms".format(output, sensor.data.gas_resistance))
-
-        else:
-            print(output)
+        output = '{0:.2f} C,{1:.2f} hPa,{2:.3f} %RH'.format(
+            sensor.data.temperature,
+            sensor.data.pressure,
+            sensor.data.humidity)
+        print(output)
 
 
 if __name__ == "__main__":
