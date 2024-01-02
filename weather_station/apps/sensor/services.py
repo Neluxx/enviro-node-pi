2ddcba8a9ce15cc529022e3d2a1d45040acaa11a
Fabian Arndt <fabian.arndt96@proton.me>
2024-01-02 15:26:29 +0100

    Remove debug functions

--- weather_station/apps/sensor/services.py
+++ weather_station/apps/sensor/services.py
@@ -38,28 +38,3 @@ class Sensor:
             co2=data["co2"],
             created=timezone.now(),
         ).save()
-
-    def debug_mhz19(self) -> None:
-        mhz19 = mh_z19.read_all()
-
-        print("MH-Z19")
-        print("CO2:", mhz19["co2"])
-
-    def debug_bme680(self) -> None:
-        try:
-            sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
-        except (RuntimeError, IOError):
-            sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
-
-        # These oversampling settings can be tweaked to change
-        # the balance between accuracy and noise in the data.
-        sensor.set_humidity_oversample(bme680.OS_2X)
-        sensor.set_pressure_oversample(bme680.OS_4X)
-        sensor.set_temperature_oversample(bme680.OS_8X)
-        sensor.set_filter(bme680.FILTER_SIZE_3)
-
-        if sensor.get_sensor_data():
-            output = "{0:.2f} C,{1:.2f} hPa,{2:.3f} %RH".format(
-                sensor.data.temperature, sensor.data.pressure, sensor.data.humidity
-            )
-            print(output)
