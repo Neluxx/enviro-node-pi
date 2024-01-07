1566236e1fd073ae5cdabc67c40f4f13f6d7232c
Fabian Arndt <fabian.arndt96@proton.me>
2024-01-07 21:15:15 +0100

    Add missing parameters

--- weather_station/apps/sensor/management/commands/sensor_data_collector.py
+++ weather_station/apps/sensor/management/commands/sensor_data_collector.py
@@ -7,7 +7,7 @@ from apps.sensor.services import Sensor
 class Command(BaseCommand):
     help = "Get sensor data"
 
-    def handle(self) -> None:
+    def handle(self, *args, **kwargs):
         sensor = Sensor()
         sensor_data = sensor.get_data()
         sensor.save_data(sensor_data)
