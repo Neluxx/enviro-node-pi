63351bfb71b719b7eaa31ffa69a8981aa0ce67e3
Fabian Arndt <fabian.arndt96@proton.me>
2024-01-07 17:38:13 +0100

    Send mail if co2 concentration is too high

--- weather_station/apps/sensor/management/commands/sensor_data_collector.py
+++ weather_station/apps/sensor/management/commands/sensor_data_collector.py
@@ -1,11 +1,25 @@
 from django.core.management.base import BaseCommand
+from django.core.mail import send_mail
+
 from apps.sensor.services import Sensor
 
 
 class Command(BaseCommand):
     help = "Get sensor data"
 
-    def handle(self, *args, **kwargs):
+    def handle(self) -> None:
         sensor = Sensor()
         sensor_data = sensor.get_data()
         sensor.save_data(sensor_data)
+
+        if sensor_data["co2"] >= 1200:
+            self.send_mail(sensor_data)
+
+    def send_mail(self, sensor_data: dict) -> None:
+        send_mail(
+            "Luftqualität ist schlecht",
+            f'Die CO2-Konzentration in der Luft beträgt {sensor_data["co2"]}. Es wird Zeit zu lüften.',
+            "fabian.arndt96@proton.me",
+            ["fabian.arndt96@proton.me"],
+            fail_silently=False,
+        )
