62f29169074fc926de6ea90ad48d30e0d3dfa042
Neluxx <fabian.arndt96@proton.me>
2025-01-19 13:14:58 +0100

    Refactor sensor data POST logic and improve error handling

--- app.py
+++ app.py
@@ -1,45 +1,43 @@
+import os
 from flask import Flask, jsonify, make_response
 from flask_restful import Resource, Api
 import requests
-
-from sensor import Sensor
 from dotenv import load_dotenv
-import os
+from sensor import Sensor
+
+CONTENT_TYPE_HEADER = {"Content-Type": "application/json"}
 
 app = Flask(__name__)
 api = Api(app)
-
 load_dotenv()
-
 
 class SensorData(Resource):
     def get(self):
         """Handle GET request to retrieve sensor data and send it to another API."""
-        sensor = Sensor()
-        data = sensor.get_data()
-        response = self.send_post_request(data)
-        return response
+        data = Sensor().get_data()
+        return self.post_sensor_data(data)
 
-    def send_post_request(self, data):
+    def post_sensor_data(self, data):
         """Send POST request with sensor data to another API."""
-        url = os.getenv("API_URL")
-        headers = {"Content-Type": "application/json"}
+        api_url = os.getenv("API_URL")
         try:
-            response = requests.post(url, json=data, headers=headers)
+            response = requests.post(api_url, json=data, headers=CONTENT_TYPE_HEADER)
             response.raise_for_status()
             return make_response(
                 jsonify({"message": "Data sent successfully"}), response.status_code
             )
-        except requests.exceptions.HTTPError as http_err:
-            return make_response(
-                jsonify({"message": f"HTTP error occurred: {http_err}"}),
-                response.status_code,
-            )
         except requests.exceptions.RequestException as req_err:
-            return make_response(
-                jsonify({"message": f"Error sending POST request: {req_err}"}), 500
-            )
+            return self.handle_request_exception(req_err, response)
 
+    def handle_request_exception(self, error, response=None):
+        """Handle exceptions during a POST request."""
+        if isinstance(error, requests.exceptions.HTTPError) and response:
+            return make_response(
+                jsonify({"message": f"HTTP error occurred: {error}"}), response.status_code
+            )
+        return make_response(
+            jsonify({"message": f"Error sending POST request: {error}"}), 500
+        )
 
 api.add_resource(SensorData, "/sensor-data")
 
