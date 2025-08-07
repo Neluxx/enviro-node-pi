081e93349f419a7377bd7dc68381dab23be0a60e
Neluxx <fabian.arndt96@proton.me>
2025-08-07 12:41:30 +0200

    Remove dashboard app and related references

--- weather_station/weather_station/settings.py
+++ weather_station/weather_station/settings.py
@@ -44,7 +44,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'apps.api',
     'apps.sensor',
-    'apps.dashboard',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
