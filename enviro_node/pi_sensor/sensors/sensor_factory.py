2667777f335ee49597bf7a44a5fe57044a888f23
Neluxx <fabian.arndt96@proton.me>
2025-08-08 21:04:33 +0200

    Add SensorFactory to manage sensor creation

---
+++ enviro_node/pi_sensor/sensors/sensor_factory.py
@@ -0,0 +1,31 @@
+import os
+from typing import Protocol
+from django.conf import settings
+
+
+class SensorProtocol(Protocol):
+    def get_data(self) -> dict: ...
+
+
+class SensorFactory:
+    @staticmethod
+    def create_bme680_sensor() -> SensorProtocol:
+        if settings.MOCK_SENSORS:
+            from pi_sensor.sensors.FakeBME680 import FakeBME680Sensor
+
+            return FakeBME680Sensor()
+        else:
+            from pi_sensor.sensors.BME680 import BME680Sensor
+
+            return BME680Sensor()
+
+    @staticmethod
+    def create_mhz19_sensor() -> SensorProtocol:
+        if settings.MOCK_SENSORS:
+            from pi_sensor.sensors.FakeMHZ19 import FakeMHZ19Sensor
+
+            return FakeMHZ19Sensor()
+        else:
+            from pi_sensor.sensors.MHZ19 import MHZ19Sensor
+
+            return MHZ19Sensor()
