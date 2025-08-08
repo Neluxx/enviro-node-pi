e1c55adad11f42ca770a48b657e126b4bc45154d
Neluxx <fabian.arndt96@proton.me>
2025-08-08 21:04:53 +0200

    Refactor sensor initialization to use SensorFactory

--- enviro_node/pi_sensor/sensors/__init__.py
+++
@@ -1,2 +0,0 @@
-from .BME680 import BME680Sensor
-from .MHZ19 import MHZ19Sensor
--- enviro_node/pi_sensor/services/sensor_data_reader.py
+++ enviro_node/pi_sensor/services/sensor_data_reader.py
@@ -1,12 +1,11 @@
-from pi_sensor.sensors.BME680 import BME680Sensor
-from pi_sensor.sensors.MHZ19 import MHZ19Sensor
+from pi_sensor.sensors.sensor_factory import SensorFactory
 
 
 class SensorDataReader:
 
     def __init__(self) -> None:
-        self.mhz19_sensor = MHZ19Sensor()
-        self.bme680_sensor = BME680Sensor()
+        self.mhz19_sensor = SensorFactory.create_mhz19_sensor()
+        self.bme680_sensor = SensorFactory.create_bme680_sensor()
 
     def get_data(self) -> dict:
         bme_data = self.bme680_sensor.get_data()
