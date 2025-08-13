c7128c74df292e618e5f0d767a690b93173a92aa
Neluxx <fabian.arndt96@proton.me>
2025-08-13 15:08:29 +0200

    Refactor API data collector to use new services

--- enviro_node/open_weather/management/commands/api_data_collector.py
+++ enviro_node/open_weather/management/commands/api_data_collector.py
@@ -1,12 +1,16 @@
 from django.core.management.base import BaseCommand
 
-from open_weather.services import OpenWeather
+from open_weather.services import OpenWeatherClient, OpenWeatherDataSaver
 
 
 class Command(BaseCommand):
     help = "Get open weather data"
 
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.open_weather_client = OpenWeatherClient()
+        self.open_weather_saver = OpenWeatherDataSaver()
+
     def handle(self, *args, **kwargs) -> None:
-        open_weather = OpenWeather()
-        open_weather_data = open_weather.get_data()
-        open_weather.save_data(open_weather_data)
+        open_weather_data = self.open_weather_client.get_current_weather()
+        self.open_weather_saver.save_data(open_weather_data)
