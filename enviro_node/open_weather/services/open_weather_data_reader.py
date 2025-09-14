dce652fd1a33b3c7ec02c0cfb3c36ad46a5b1540
Neluxx <fabian.arndt96@proton.me>
2025-09-14 20:51:08 +0200

    Order unsubmitted OpenWeather data by creation time

--- enviro_node/open_weather/services/open_weather_data_reader.py
+++ enviro_node/open_weather/services/open_weather_data_reader.py
@@ -7,7 +7,9 @@ from open_weather.models import OutdoorWeatherData
 class OpenWeatherReader:
 
     def get_unsubmitted_data(self) -> QuerySet[OutdoorWeatherData]:
-        return OutdoorWeatherData.objects.filter(submitted_at=None)
+        return OutdoorWeatherData.objects.filter(submitted_at=None).order_by(
+            "created_at"
+        )
 
     def mark_as_submitted(self, queryset: QuerySet[OutdoorWeatherData]) -> int:
         return queryset.update(submitted_at=timezone.now())
