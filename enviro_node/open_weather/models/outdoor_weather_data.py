c6f730ac48052d0d057748c2dc189b7377cb5b86
Neluxx <fabian.arndt96@proton.me>
2025-09-17 21:15:52 +0200

    Use constants for open weather field validators

--- enviro_node/open_weather/models/outdoor_weather_data.py
+++ enviro_node/open_weather/models/outdoor_weather_data.py
@@ -3,43 +3,31 @@ from typing import Any
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
+TEMPERATURE_VALIDATORS = [MinValueValidator(-100), MaxValueValidator(100)]
+PERCENTAGE_VALIDATORS = [MinValueValidator(0), MaxValueValidator(100)]  # In percentage
+PRESSURE_VALIDATORS = [MinValueValidator(900), MaxValueValidator(1100)]  # In hPa
+VISIBILITY_VALIDATORS = [MinValueValidator(0), MaxValueValidator(10000)]  # In meter
+WIND_SPEED_VALIDATORS = [MinValueValidator(0), MaxValueValidator(150)]  # In m/s
+WIND_DEGREE_VALIDATORS = [MinValueValidator(0), MaxValueValidator(360)]  # In degree
+
 
 class OutdoorWeatherData(models.Model):
-    temperature = models.FloatField(
-        validators=[MinValueValidator(-100), MaxValueValidator(70)]
-    )
-    feels_like = models.FloatField(
-        validators=[MinValueValidator(-100), MaxValueValidator(70)]
-    )
-    temp_min = models.FloatField(
-        validators=[MinValueValidator(-100), MaxValueValidator(70)]
-    )
-    temp_max = models.FloatField(
-        validators=[MinValueValidator(-100), MaxValueValidator(70)]
-    )
-    humidity = models.IntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(100)]
-    )
-    pressure = models.IntegerField(
-        validators=[MinValueValidator(500), MaxValueValidator(1200)]
-    )
+    temperature = models.FloatField(validators=TEMPERATURE_VALIDATORS)
+    feels_like = models.FloatField(validators=TEMPERATURE_VALIDATORS)
+    temp_min = models.FloatField(validators=TEMPERATURE_VALIDATORS)
+    temp_max = models.FloatField(validators=TEMPERATURE_VALIDATORS)
+    humidity = models.IntegerField(validators=PERCENTAGE_VALIDATORS)
+    pressure = models.IntegerField(validators=PRESSURE_VALIDATORS)
 
     weather_main = models.CharField(max_length=255, blank=False)
     weather_description = models.CharField(max_length=255, blank=False)
     weather_icon = models.CharField(max_length=255, blank=False)
 
-    visibility = models.IntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(50000)]
-    )
-    wind_speed = models.FloatField(
-        validators=[MinValueValidator(0), MaxValueValidator(200)]
-    )
-    wind_deg = models.IntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(360)]
-    )
-    clouds = models.IntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(100)]
-    )
+    visibility = models.IntegerField(validators=VISIBILITY_VALIDATORS)
+    wind_speed = models.FloatField(validators=WIND_SPEED_VALIDATORS)
+    wind_deg = models.IntegerField(validators=WIND_DEGREE_VALIDATORS)
+    clouds = models.IntegerField(validators=PERCENTAGE_VALIDATORS)
+
     submitted_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
