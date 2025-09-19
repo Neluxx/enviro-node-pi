c856169358bebe5ef9809446d9b3d57e09ad0517
Neluxx <fabian.arndt96@proton.me>
2025-09-19 15:09:50 +0200

    Improve sensor variable names

--- enviro_node/pi_sensor/services/sensor_reader.py
+++ enviro_node/pi_sensor/services/sensor_reader.py
@@ -4,10 +4,11 @@ from pi_sensor.sensors.sensor_factory import SensorFactory
 class SensorReader:
 
     def __init__(self) -> None:
-        self.mhz19_sensor = SensorFactory.create_mhz19_sensor()
-        self.bme680_sensor = SensorFactory.create_bme680_sensor()
+        self.co2_sensor = SensorFactory.create_mhz19_sensor()
+        self.environmental_sensor = SensorFactory.create_bme680_sensor()
 
     def collect_data(self) -> dict[str, float]:
-        bme_data = self.bme680_sensor.get_data()
-        co2_data = self.mhz19_sensor.get_data()
-        return {**bme_data, **co2_data}
+        environmental_data = self.environmental_sensor.get_data()
+        co2_data = self.co2_sensor.get_data()
+
+        return {**environmental_data, **co2_data}
