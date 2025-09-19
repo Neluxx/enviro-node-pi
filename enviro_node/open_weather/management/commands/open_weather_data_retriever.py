0c01b17d1cda1d1f63f0e7439b931a4333d1428a
Neluxx <fabian.arndt96@proton.me>
2025-09-19 14:41:00 +0200

    Add logging and error handling to open weather data retriever command

---
+++ enviro_node/open_weather/management/commands/open_weather_data_retriever.py
@@ -0,0 +1,32 @@
+import logging
+
+from django.core.management.base import BaseCommand
+
+from open_weather.services import OpenWeatherClient, OpenWeatherRepository
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    help = "Retrieve current weather data from open weather API"
+
+    def handle(self, *args, **kwargs) -> None:
+        logger.info("Starting open weather data retrieving")
+
+        try:
+            open_weather_client = OpenWeatherClient()
+            repository = OpenWeatherRepository()
+
+            logger.info("Retrieve current open weather data...")
+            open_weather_data = open_weather_client.get_current_weather()
+            logger.info(f"Retrieved current open weather data: {open_weather_data}")
+
+            logger.info("Storing open weather data to database...")
+            repository.insert(open_weather_data)
+
+        except Exception as e:
+            logger.error(f"Error during open weather data retrieving: {e}")
+            raise
+
+        finally:
+            logger.info("Open weather data retrieving completed successfully")
