b272385df3ec5e6881c6159208713d9b34b3a718
Neluxx <fabian.arndt96@proton.me>
2025-09-21 20:07:10 +0200

    Add BaseSensor abstract class for sensor abstraction

---
+++ enviro_node/pi_sensor/sensors/base_sensor.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+
+
+class BaseSensor(ABC):
+    """Base sensor class with common functionality"""
+
+    @abstractmethod
+    def get_data(self) -> dict[str, float]:
+        """Get sensor data"""
+        pass
+
+    def __enter__(self) -> "BaseSensor":
+        """Context manager entry"""
+        return self
